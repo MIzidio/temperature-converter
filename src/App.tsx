fe238d6cb1565a78a08139f0523b1bbacef43903
Matheus <matheusimeira@gmail.com>
2023-09-08 00:29:49 -0300

    temperature converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [temperatureF, setTemperatureF] = useState<number | null>(null)
+  const [temperatureC, setTemperatureC] = useState<number | null>(null)
+  const [temperatureK, setTemperatureK] = useState<number | null>(null)
+
+  const celciusToFahrenheit = (temperature: number) => {
+    return temperature * 1.8 + 32
+  }
+
+  const fahrenheitToCelcius = (temperature: number) => {
+    return (temperature - 32) * (5/9)
+  }
+
+  const celciusToKelvin = (temperature: number) => {
+    return Number(temperature) + 273.15
+  }
+
+  const kelvinToCelcius = (temperature: number) => {
+    return temperature - 273.15
+  }
+
+  const fahrenheitToKelvin = (temperature: number) => {
+    return (temperature - 32) * (5/9) + 273.15
+  }
+
+  const kelvinToFahrenheit = (temperature: number) => {
+    return (temperature - 273.15) * 1.8 + 32
+  }
+
+  const handleCelciusChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const numberTemperature = event.target.value as unknown as number
+
+    setTemperatureC(numberTemperature)
+    setTemperatureF(celciusToFahrenheit(numberTemperature))
+    setTemperatureK(celciusToKelvin(numberTemperature))
+  }
+
+  const handleFahrenheitChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const numberTemperature = event.target.value as unknown as number
+
+    setTemperatureF(numberTemperature)
+    setTemperatureC(fahrenheitToCelcius(numberTemperature))
+    setTemperatureK(fahrenheitToKelvin(numberTemperature))
+  }
+
+  const handleKelvinChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const numberTemperature = event.target.value as unknown as number
+
+    setTemperatureK(numberTemperature)
+    setTemperatureC(kelvinToCelcius(numberTemperature))
+    setTemperatureF(kelvinToFahrenheit(numberTemperature))
+  }
 
   return (
     <>
@@ -18,12 +68,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          A contagem está em {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className='temperature-box'>
+          <label>Celcius</label>
+          <input value={temperatureC ?? ''} onChange={handleCelciusChange} name='celcius' type='number'/>
+        </div>
+        <div className='temperature-box'>
+          <label htmlFor="">Fahrenheit</label>
+          <input value={temperatureF ?? ''} onChange={handleFahrenheitChange} name='fahrenheit' type='number'/>
+        </div>
+        <div className='temperature-box'>
+          <label htmlFor="">Kelvin</label>
+          <input value={temperatureK ?? ''} onChange={handleKelvinChange} name='kelvin' type='number'/>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
