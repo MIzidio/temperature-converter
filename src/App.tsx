1569103724012dd2e96712501af69e12dd1ee60d
Matheus <matheusimeira@gmail.com>
2023-09-27 23:35:41 -0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { ChangeEvent, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -57,34 +55,20 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <div className='temperature-box'>
+        <label>Celcius</label>
+        <input value={temperatureC ?? ''} onChange={handleCelciusChange} name='celcius' type='number'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <div className='temperature-box'>
-          <label>Celcius</label>
-          <input value={temperatureC ?? ''} onChange={handleCelciusChange} name='celcius' type='number'/>
-        </div>
-        <div className='temperature-box'>
-          <label htmlFor="">Fahrenheit</label>
-          <input value={temperatureF ?? ''} onChange={handleFahrenheitChange} name='fahrenheit' type='number'/>
-        </div>
-        <div className='temperature-box'>
-          <label htmlFor="">Kelvin</label>
-          <input value={temperatureK ?? ''} onChange={handleKelvinChange} name='kelvin' type='number'/>
-        </div>
+      <div className='temperature-box'>
+        <label>Fahrenheit</label>
+        <input value={temperatureF ?? ''} onChange={handleFahrenheitChange} name='fahrenheit' type='number'/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='temperature-box'>
+        <label>Kelvin</label>
+        <input value={temperatureK ?? ''} onChange={handleKelvinChange} name='kelvin' type='number'/>
+      </div>
+    </div>
   )
 }
 
